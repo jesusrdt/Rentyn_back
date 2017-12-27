4d08a8e8a52aec16f1476e6dc77a5a211e4550c9
Jorge Villasmil <32938328+keine1lust@users.noreply.github.com>
2017-12-27 10:53:32 -0400

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('contact', 'ContactController@store');
+Route::post('/v1/contact/form', 'ContactController@store');
