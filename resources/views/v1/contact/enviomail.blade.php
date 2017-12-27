0c302733a41152fae0073986529769655d3ae0fe
Jorge Villasmil <32938328+keine1lust@users.noreply.github.com>
2017-12-27 10:21:15 -0400

    vista del envio de correo
    la vista donde se cargan los datos enviados de Mail/Contactmail que se mostrara en el correo

---
+++ resources/views/v1/contact/enviomail.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewmail">
+    <title>Solicitud</title>
+</head>
+<body>
+
+<p><b>Que Busco: </b>{{ $que }} </p>
+
+<p><b>Cuando desde: </b>{{ $cuando }} Hasta </b>{{ $cuando }} </p>
+
+<p><b>Donde </b>{{ $donde }} </p>
+
+
+<p><b> Datos del Solicitante </b> </p>
+<ul>
+<li><b>Nombre: </b>{{ $nombre }} </li>
+<li><b>Telefono: </b>{{ $telefono }} </li>
+<li><b>Correo: </b>{{ $correo }} </li>
+</ul>
+</body>
+</html>
+
