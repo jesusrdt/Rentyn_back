6c73d79fc5c592007e5acd0c06ef4b4d2f0edbda
Gabriel leal <gaboleal123@gmail.com>
2017-12-27 10:56:38 -0430

    Aginación #1 y #2
    Maquetacion Index-Info más landing page

---
+++ resources/views/elements/formCorreo.blade.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+   <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+  
+  <title>{{ config('app.name', 'Laravel') }}</title>
+  
+  <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet"> 
+  <link href="{{ asset('css/landing.css') }}" rel="stylesheet">
+
+<style type="text/css" media="screen">
+
+
+datalist{
+	border-color: black;
+}
+
+
+
+
+
+.form-row input{
+	background-color: #ededed;
+	width: 350px;
+	height: 30px;
+    border-style:none;
+    margin-right: 20px;
+  
+}
+
+label{
+
+	color: #abacad;
+	margin: 15px;
+  text-align: left;
+}
+
+
+
+</style>
+
+
+
+</head>
+<body >
+	
+ <!-- ==================== barra header ============================== -->
+  <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+          <div class="row">
+            <div class="col-xs-12 col-sm-5 col-sm-offset-1">
+              <img  class="img-logo" src="{{ asset('img/logo.jpg') }}"  alt="">
+            <button type="button" class="navbar-toggle" data-toggle="collapse"></button>
+            <a href="#" class="navbar-brand"></a>
+            </div>  
+          </div>
+      </div>
+  </nav>
+
+ <header class="header-co">
+  
+  <div class="container">
+    <div class="col-sm-4 col-sm-offset-2">
+    
+    </div>
+  </div>
+
+ </header>
+
+ <!-- ================================================================ -->
+
+    
+<div class="container">
+  <div class="row centrado">
+    <div class="col-lg-6 col-lg-offset-3">
+	     <h2>4.- Déjanos tus datos </h2>
+          <div class="col-md-3" >
+             <div class="main-content">
+
+               <form class="form-register" method="post" action="#">
+                 <div class="form-row">
+                        <label>
+                            <span>Nombre</span><br>
+                            <input type="text" name="name" required="">
+                        </label>
+                  </div>
+
+                  <div class="form-row">
+                        <label>
+                            <span>Teléfono</span><br>
+                            <input type="tel" name="telefono" required="">
+                        </label>
+                  </div>
+
+                  <div class="form-row">
+                        <label>
+                            <span>Email</span><br>
+                            <input type="email" name="email" required="">
+                        </label>
+                  </div>
+              </form>
+            </div>
+          </div>  
+          <div class="col-md-3" >
+	         <input id="enviar" type="submit" value="Enviar">
+          </div>
+       </div>
+    </div>
+  </div>  
+</body>
+</html>
---
+++ resources/views/elements/formSearch.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+	<title>Rentyn</title>
+</head>
+<body>
+	
+  <header>
+  <div width="700px" heigth="100" style={ background-color:"black";}>
+  		<img src="" alt="">
+  	</div>
+
+  </header>
+
+<div class="box">
+  <div class="container-1">
+      <span class="icon"><i class="fa fa-search"></i></span>
+      <input type="search" id="search" placeholder="Search..." />
+  </div>
+</div>
+
+</body>
+</html>
