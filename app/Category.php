cf38e302cad8b7fdba428ebc9a8e3b2f35edc1a8
Miguel Lobo <wmaster732@gmail.com>
2017-12-28 16:14:41 +0000

    relacion categories con categories

---
+++ app/Category.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $table = 'categories';
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'description', 'imagen', 'status',
+    ];
+
+    public function subCategories()
+    {
+        return $this->hasMany('App\SubCategory');
+    }
+
+
+    public function subCategory()
+    {
+        return $this->belongsTo('App\SubCategory');
+    }
+}
---
+++ app/SubCategory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SubCategory extends Model
+{
+    protected $table = 'sub_categories';
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'category_id', 'subcategory_id',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
+
+    public function info()
+    {
+        return $this->hasOne('App\Category', 'id', 'subcategory_id');
+    }
+}
