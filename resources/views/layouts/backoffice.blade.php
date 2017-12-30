3c8a3bd9a77e594e1532a5c08c8dc235e3264f86
jesus <serviciosdpc@gmail.com>
2017-12-29 23:00:25 -0400

    crear layout backoffice y estructurar para que sea reusable + carpeta views/backoffice

---
+++ resources/views/layouts/backoffice.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ backoffice()->getLocale() }}">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>{{ config('app.name', 'Laravel') }}</title>
+</head>
+<body>
+	<!--header-->
+	@include('header')
+	<!--end header-->
+	<!--navbar-->
+	@include('navbar')
+	<!--end navbar-->
+	@yield('content')
+	<!--footer-->
+	@include('footer')
+	<!-- end footer-->
+</body>
+</html>
