dd1313adca7380e3b788df21f8a09c37eb933003
Omar Ramirez <onmigregor@gmail.com>
2018-01-02 18:29:35 -0400

    Asunto:cambio estilo tabla

---
+++ resources/views/admin/reference-products.blade.php
@@ -0,0 +1,72 @@
+	
+
+<link href="http://designers.hubspot.com/hs-fs/hub/327485/file-2054199286-css/font-awesome.css"En Inglés rel="stylesheet">
+
+
+
+
+
+			<div class="container">
+			  <h2>Lista de Productos</h2>            
+			  <table class="table 	table-striped">
+			    <thead>
+			      <tr>
+			        <th>Nº Refrencia</th>
+			        <th>Producto</th>
+			        <th>Categoria</th>
+			        <th></th>
+			      </tr>
+			    </thead>
+			    <tbody>
+
+			      <tr>
+			        <td>1545154</td>
+			        <td>Excavadora</td>
+			        <td>Maquinaria</td>
+			        <td align="center">			        	
+  							<a href=""><i style="margin:  10px;" title="Editar" class="fa fa-pencil fa-1x"></i></a>
+			        		<a href=""><i style="margin:  10px;" title="Eliminar" class="fa fa-trash-o fa-1x"></i></a>						
+					</td>
+			      </tr>
+			      <tr>
+			        <td>1545147</td>
+			        <td>mezcladora</td>
+			        <td>Maquinaria</td>
+			        <td align="center">
+			        	<a href=""><i  style="margin:  10px;" title="Editar" class="fa fa-pencil fa-1x"></i></a>
+			        	<a href=""><i  style="margin:  10px;" title="Eliminar" class="fa fa-trash-o fa-1x"></i></a>
+					</td>
+			      </tr>
+			    </tbody>
+			  </table>
+
+
+
+				 <ul class="pagination">
+	  <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+	  <li class="page-item"><a class="page-link" href="#">1</a></li>
+	  <li class="page-item active"><a class="page-link" href="#">2</a></li>
+	  <li class="page-item"><a class="page-link" href="#">3</a></li>
+	  <li class="page-item"><a class="page-link" href="#">Next</a></li>
+	</ul> 
+			</div>
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
