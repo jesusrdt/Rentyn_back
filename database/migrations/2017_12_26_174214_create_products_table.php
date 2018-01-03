754211fbae8a4d74063cea1fc6ada2e4e97e7b9d
Jorge Villasmil <32938328+keine1lust@users.noreply.github.com>
2018-01-03 10:20:10 -0400

    migracion para product con soft_delete
    migracion para product con soft_delete

---
+++ database/migrations/2017_12_26_174214_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+			$table->string('title', 50)->nullable();
+			$table->string('characteristic', 254)->nullable();
+			$table->longText('description', 254);
+			$table->string('imagen')->nullable();
+			$table->char("status", 1);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
