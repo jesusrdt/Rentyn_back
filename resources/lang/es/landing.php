228dfa185a226fc61393cda0fa15d8609300d3cb
Jorge Villasmil <32938328+keine1lust@users.noreply.github.com>
2018-01-04 14:36:42 -0400

    Add files via upload

---
+++ resources/lang/es/landing.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'what'   => '¿Que necesitas?',
+    'where' => '¿Donde estas?',
+    'when' => '¿Cuando lo necesitas?',
+    'name' => 'Tu Nombre',
+    'phone' => 'Tu teléfono',
+    'email' => 'Tu correo electronico',
+    'msg' => '¡No se preocupes y cuente con nosotros!',
+    'msg2' => 'Alquilar a través de rentyn significa tener disponible la maquinaria que necesita justo cuando la necesita.'
+];
