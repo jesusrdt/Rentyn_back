f2d1bda7f6e675488dd28ac48a946eb537b7be80
Jorge Villasmil <32938328+keine1lust@users.noreply.github.com>
2018-01-04 14:35:30 -0400

    Add files via upload

---
+++ resources/lang/en/landing.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    'what'   => 'what do yo need?',
+    'where' => 'Where are you?',
+    'when' => 'When you need it?',
+    'name' => 'Name',
+    'phone' => 'Phone',
+    'email' => 'Email',
+    'msg' => 'Do not worry and count on us!',
+    'msg2' =>'Renting through rentyn means having the machinery you need available just when you need it'  
+
+];
