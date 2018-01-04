0d5c7c47373bdf2405764b4a20c8fcec5897146b
Omar Ramirez <onmigregor@gmail.com>
2018-01-04 10:53:23 -0400

    Asunto:Agregar Vistas productos index formulario

---
+++ resources/views/admin/reference-products.blade.php
@@ -0,0 +1,164 @@
+	@extends('layouts.app')
+
+
+<link href="http://designers.hubspot.com/hs-fs/hub/327485/file-2054199286-css/font-awesome.css"En Inglés rel="stylesheet">
+
+<style type="text/css">
+body {
+    background: #dfdfdf;
+    color: #666;
+    font: 14px/24px sans-serif;
+}
+
+#container {
+    width: 600px;
+    margin: 0 auto;
+    padding: 20px;
+}
+
+.btn {
+    display: inline-block;
+    padding: 10px;
+    border-radius: 5px; /*optional*/
+    color: #aaa;
+    font-size: .875em;
+}
+
+.pagination {
+    background: #f2f2f2;
+    padding: 20px;
+    margin-bottom: 20px;
+}
+
+.page {
+    display: inline-block;
+    padding: 0px 9px;
+    margin-right: 4px;
+    border-radius: 3px;
+    border: solid 1px #c0c0c0;
+    background: #e9e9e9;
+    box-shadow: inset 0px 1px 0px rgba(255,255,255, .8), 0px 1px 3px rgba(0,0,0, .1);
+    font-size: .875em;
+    font-weight: bold;
+    text-decoration: none;
+    color: #717171;
+    text-shadow: 0px 1px 0px rgba(255,255,255, 1);
+}
+
+.page:hover, .page.gradient:hover {
+    background: #fefefe;
+    background: -webkit-gradient(linear, 0% 0%, 0% 100%, from(#FEFEFE), to(#f0f0f0));
+    background: -moz-linear-gradient(0% 0% 270deg,#FEFEFE, #f0f0f0);
+}
+
+.page.active {
+    border: none;
+    background: #000000;
+    box-shadow: inset 0px 0px 8px rgba(0,0,0, .5), 0px 1px 0px rgba(255,255,255, .8);
+    color: #f0f0f0;
+    text-shadow: 0px 0px 3px rgba(0,0,0, .5);
+}
+
+.page.gradient {
+    background: -webkit-gradient(linear, 0% 0%, 0% 100%, from(#f8f8f8), to(#e9e9e9));
+    background: -moz-linear-gradient(0% 0% 270deg,#f8f8f8, #e9e9e9);
+}
+
+.pagination.dark {
+    background: #212529;
+    color: #feffff;
+}
+
+.page.dark {
+    border: solid 1px #32373b;
+    background: #3e4347;
+    box-shadow: inset 0px 1px 1px rgba(255,255,255, .1), 0px 1px 3px rgba(0,0,0, .1);
+    color: #feffff;
+    text-shadow: 0px 1px 0px rgba(0,0,0, .5);
+}
+
+.page.dark:hover, .page.dark.gradient:hover {
+    background: #3d4f5d;
+    background: -webkit-gradient(linear, 0% 0%, 0% 100%, from(#547085), to(#3d4f5d));
+    background: -moz-linear-gradient(0% 0% 270deg,#547085, #3d4f5d);
+}
+
+.page.dark.active {
+    border: none;
+    background: #2f3237;
+    box-shadow: inset 0px 0px 8px rgba(0,0,0, .5), 0px 1px 0px rgba(255,255,255, .1);
+}
+
+.page.dark.gradient {
+    background: -webkit-gradient(linear, 0% 0%, 0% 100%, from(#565b5f), to(#3e4347));
+    background: -moz-linear-gradient(0% 0% 270deg,#565b5f, #3e4347);
+}
+</style>
+
+
+			<div class="container">
+			  <h2>Lista de Productos</h2>            
+			  <table class="table table-dark table-striped ">
+			    <thead>
+			      <tr>
+			        <th>Nº Refrencia</th>
+			        <th>Producto</th>
+			        <th>Categoria</th>
+			        <th></th>
+			      </tr>
+			    </thead>
+			    <tbody>
+
+			      <tr>
+			        <td>1545154</td>
+			        <td>Excavadora</td>
+			        <td>Maquinaria</td>
+			        <td align="center">
+			        		<a href=""><i  style="margin:  10px;" title="Ver Informacion" 		class="fa fa-eye fa-1x"></i></a>       	
+  							<a href=""><i style="margin:  10px;" title="Editar" class="fa fa-pencil fa-1x"></i></a>
+			        		<a href=""><i style="margin:  10px;" title="Eliminar" class="fa fa-trash-o fa-1x"></i></a>						
+					</td>
+			      </tr>
+			      <tr>
+			        <td>1545147</td>
+			        <td>mezcladora</td>
+			        <td>Maquinaria</td>
+			        <td align="center">
+			        	<a href=""><i  style="margin:  10px;" title="Ver Informacion" 		class="fa fa-eye fa-1x"></i></a>
+			        	<a href=""><i  style="margin:  10px;" title="Editar" 	class="fa fa-pencil fa-1x"></i></a>
+			        	<a href=""><i  style="margin:  10px;" title="Eliminar" 	class="fa fa-trash-o fa-1x"></i></a>
+					</td>
+			      </tr>
+			    </tbody>
+			  </table>
+					<ul class="pagination dark justify-content-center	">
+					  <li class="page-item  dark"><a class="page-link page dark" href="#">Previous</a></li>
+					  <li class="page-item  dark"><a class="page-link page dark" href="#">1</a></li>
+					  <li class="page-item  dark"><a class="page-link page active" href="#">2</a></li>
+					  <li class="page-item  dark"><a class="page-link page dark" href="#">3</a></li>
+					  <li class="page-item  dark"><a class="page-link page dark" href="#">4</a></li>
+					  <li class="page-item  dark"><a class="page-link page dark" href="#">Next</a></li>
+					</ul>
+
+
+</div>
+	
+
+<!-- 		<a href="#" class="page dark">first</a>
+		<a href="#" class="page dark">2</a>
+		<a href="#" class="page dark">3</a>
+		<span class="page dark active">4</span>
+		<a href="#" class="page dark">5</a>
+		<a href="#" class="page dark">6</a>
+		<a href="#" class="page dark">last</a>
+
+
+
+ -->
+
+
+
+
+
+
+
